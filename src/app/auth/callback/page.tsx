aa90b343c46fe311fb4b1ddeb83f200e800aac50
Yathish G P <yathishgp1@gmail.com>
2025-02-17 20:23:21 +0530

    added some more functions to authentication

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,14 +1,14 @@
-import { onAuthenticateUser } from '@/actions/user'
-import { redirect } from 'next/navigation'
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 
 const AuthCallbackPage = async () => {
-  const auth = await onAuthenticateUser()
-  console.log(auth)
+  const auth = await onAuthenticateUser();
+
   if (auth.status === 200 || auth.status === 201)
-    return redirect(`/dashboard/${auth.user?.workspace[0].id}`)
+    return redirect(`/dashboard/${auth.user?.workspace[0].id}`);
 
   if (auth.status === 403 || auth.status === 400 || auth.status === 500)
-    return redirect('/auth/sign-in')
-}
+    return redirect("/auth/sign-in");
+};
 
-export default AuthCallbackPage
+export default AuthCallbackPage;
