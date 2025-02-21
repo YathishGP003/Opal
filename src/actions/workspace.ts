1e51881b1df02713bedd3c7c05b0892309042dde
Yathish G P <yathishgp1@gmail.com>
2025-02-21 13:54:52 +0530

    added actions realted payment and notifications

--- src/actions/workspace.ts
+++ src/actions/workspace.ts
@@ -2,6 +2,10 @@
 
 import { client } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
+import { sendEmail } from "./user";
+// import { createClient, OAuthStrategy } from '@wix/sdk'
+// import { items } from '@wix/data'
+// import axios from 'axios'
 
 export const verifyAccessToWorkspace = async (workspaceId: string) => {
   try {
@@ -266,3 +270,228 @@ export const getFolderInfo = async (folderId: string) => {
     };
   }
 };
+
+export const moveVideoLocation = async (
+  videoId: string,
+  workSpaceId: string,
+  folderId: string
+) => {
+  try {
+    const location = await client.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        folderId: folderId || null,
+        workSpaceId,
+      },
+    });
+    if (location) return { status: 200, data: "folder changed successfully" };
+    return { status: 404, data: "workspace/folder not found" };
+  } catch (error) {
+    return { status: 500, data: "Oops! something went wrong" };
+  }
+};
+
+export const getPreviewVideo = async (videoId: string) => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+    const video = await client.video.findUnique({
+      where: {
+        id: videoId,
+      },
+      select: {
+        title: true,
+        createdAt: true,
+        source: true,
+        description: true,
+        processing: true,
+        views: true,
+        summery: true,
+        User: {
+          select: {
+            firstname: true,
+            lastname: true,
+            image: true,
+            clerkid: true,
+            trial: true,
+            subscription: {
+              select: {
+                plan: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    if (video) {
+      return {
+        status: 200,
+        data: video,
+        author: user.id === video.User?.clerkid ? true : false,
+      };
+    }
+
+    return { status: 404 };
+  } catch (error) {
+    return { status: 400 };
+  }
+};
+
+export const sendEmailForFirstView = async (videoId: string) => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+    const firstViewSettings = await client.user.findUnique({
+      where: { clerkid: user.id },
+      select: {
+        firstView: true,
+      },
+    });
+    if (!firstViewSettings?.firstView) return;
+
+    const video = await client.video.findUnique({
+      where: {
+        id: videoId,
+      },
+      select: {
+        title: true,
+        views: true,
+        User: {
+          select: {
+            email: true,
+          },
+        },
+      },
+    });
+    if (video && video.views === 0) {
+      await client.video.update({
+        where: {
+          id: videoId,
+        },
+        data: {
+          views: video.views + 1,
+        },
+      });
+
+      const { transporter, mailOptions } = await sendEmail(
+        video.User?.email!,
+        "You got a viewer",
+        `Your video ${video.title} just got its first viewer`
+      );
+
+      transporter.sendMail(mailOptions, async (error, info) => {
+        if (error) {
+          console.log(error.message);
+        } else {
+          const notification = await client.user.update({
+            where: { clerkid: user.id },
+            data: {
+              notification: {
+                create: {
+                  content: mailOptions.text,
+                },
+              },
+            },
+          });
+          if (notification) {
+            return { status: 200 };
+          }
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// export const editVideoInfo = async (
+//   videoId: string,
+//   title: string,
+//   description: string
+// ) => {
+//   try {
+//     const video = await client.video.update({
+//       where: { id: videoId },
+//       data: {
+//         title,
+//         description,
+//       },
+//     })
+//     if (video) return { status: 200, data: 'Video successfully updated' }
+//     return { status: 404, data: 'Video not found' }
+//   } catch (error) {
+//     return { status: 400 }
+//   }
+// }
+
+// export const getWixContent = async () => {
+//   try {
+//     const myWixClient = createClient({
+//       modules: { items },
+//       auth: OAuthStrategy({
+//         clientId: process.env.WIX_OAUTH_KEY as string,
+//       }),
+//     })
+
+//     const videos = await myWixClient.items
+//       .queryDataItems({
+//         dataCollectionId: 'opal-videos',
+//       })
+//       .find()
+
+//     const videoIds = videos.items.map((v) => v.data?.title)
+
+//     const video = await client.video.findMany({
+//       where: {
+//         id: {
+//           in: videoIds,
+//         },
+//       },
+//       select: {
+//         id: true,
+//         createdAt: true,
+//         title: true,
+//         source: true,
+//         processing: true,
+//         workSpaceId: true,
+//         User: {
+//           select: {
+//             firstname: true,
+//             lastname: true,
+//             image: true,
+//           },
+//         },
+//         Folder: {
+//           select: {
+//             id: true,
+//             name: true,
+//           },
+//         },
+//       },
+//     })
+
+//     if (video && video.length > 0) {
+//       return { status: 200, data: video }
+//     }
+//     return { status: 404 }
+//   } catch (error) {
+//     console.log(error)
+//     return { status: 400 }
+//   }
+// }
+
+// export const howToPost = async () => {
+//   try {
+//     const response = await axios.get(process.env.CLOUD_WAYS_POST as string)
+//     if (response.data) {
+//       return {
+//         title: response.data[0].title.rendered,
+//         content: response.data[0].content.rendered,
+//       }
+//     }
+//   } catch (error) {
+//     return { status: 400 }
+//   }
+// }
